7472ff1d97f91a7610440fc181feaf04257dd6b7
princed3rpy <64145381+princed3rpy@users.noreply.github.com>
2022-05-15 11:08:52 +0700

    Latihan C++

---
+++ Latihan C++ - 1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a,b;
+    cout<<"Masukkan sebuah bilangan: ";
+    cin>>a;
+    cout<<"\nMasukkkan bilangan pembagi: ";
+    cin>>b;
+    float c=a/b;
+    cout<<"\nHasilnya adalah: "<<c;
+    
+    return 0;
+}
---
+++ Latihan C++ - 2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namesp
+ace std;
+
+int main(){
+    for(int i = 0; i < 10; i++){
+    printf("Perulangan ke-%i\n", i);
+    }
+
+    return 0;
+}
