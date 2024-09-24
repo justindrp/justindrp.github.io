eeda294d6808d55c1332c3626a4fe9564d5f7273
Your Name <you@example.com>
2024-09-24 10:52:27 +0700

    Revised items

--- test02.js
+++ test02.js
@@ -1,4 +1,45 @@
-function myFunction() {
-    document.getElementById("test").innerHTML = "Hi there!"
-}
+function getAverage(scores) {
+    let sum = 0;
+  
+    for (const score of scores) {
+      sum += score;
+    }
+  
+    return sum / scores.length;
+  }
+  
+  function getGrade(score) {
+    if (score === 100) {
+      return "A++";
+    } else if (score >= 90) {
+      return "A";
+    } else if (score >= 80) {
+      return "B";
+    } else if (score >= 70) {
+      return "C";
+    } else if (score >= 60) {
+      return "D";
+    } else {
+      return "F";
+    }
+  }
+  
+  function hasPassingGrade(score) {
+    return getGrade(score) !== "F";
+  }
 
+  function studentMsg(totalScores, studentScore) {
+    let passingMessage;
+    if (hasPassingGrade(studentScore)) {
+        passingMessage = "You passed the course.";
+    } else {
+        passingMessage = "You failed the course.";
+    }
+    const message = "Class average: " + getAverage(totalScores) + ". Your grade: " + getGrade(studentScore) + ". " + passingMessage;
+    return message;
+  }
+
+  function updateMessage() {
+    const message = studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37);
+    document.getElementById("message").innerHTML = message;
+  }
